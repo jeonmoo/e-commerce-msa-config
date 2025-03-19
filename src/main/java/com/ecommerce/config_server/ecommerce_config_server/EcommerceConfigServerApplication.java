21a3f7c8314cdf5e375929fa4be90e52a62d1020
jeonmoo <rjm0710@naver.com>
2025-03-19 21:58:09 +0900

    docs: config 서버 생성

---
+++ src/main/java/com/ecommerce/config_server/ecommerce_config_server/EcommerceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.config_server.ecommerce_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/config_server/ecommerce_config_server/EcommerceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.config_server.ecommerce_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
